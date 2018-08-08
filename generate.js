5c3fe39c067924bae32f97534fe5e83422b12d3f
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-08 10:33:46 -0700

    Implement MVP data generation script

--- generate.js
+++ generate.js
@@ -1,12 +1,9 @@
-const mongoose = require('mongoose');
 const faker = require('faker');
-// const path = require('path');
-
-const db = require('./models/');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const IMG_URL = 'https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-';
 
-const MAX_ID_RANGE = 10000000;
+const MAX_ID_RANGE = 100000;
 const MAX_IMG_RANGE = 10;
 
 const getRandomIntInclusive = (min, max) => {
@@ -14,46 +11,36 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - minRounded + 1)) + minRounded;
 };
 
-// drop collection if exists
-console.log('cleared db for re-seed...\n');
-db.Photo.remove({}).exec((err) => {
-  if (err) {
-    console.error(err);
-    return process.exit(0);
+console.log('starting data generation....\n');
+
+const data = [];
+
+for (let id = 1; id <= MAX_ID_RANGE; id += 1) {
+  for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
+    const photo = {
+      room_id: id,
+      photo_url: `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`,
+      verified: !Math.floor(Math.random() * 2),
+      description: faker.lorem.sentence(),
+    };
+    data.push(photo);
   }
+}
 
-  console.log('db cleared!\n');
-  console.log('starting re-seeding....\n');
-
-  const inProgressDataBaseEntries = [];
-
-  for (let id = 1; id <= MAX_ID_RANGE; id += 1) {
-    for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
-      const photo = new db.Photo({
-        room_id: id,
-        photo_url: `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`,
-        verified: !Math.floor(Math.random() * 2),
-        description: faker.lorem.sentence(),
-      });
-
-      const result = photo.save().then((item) => {
-        Promise.resolve(item);
-      });
-
-      inProgressDataBaseEntries.push(result);
-    }
-  }
-
-  Promise.all(inProgressDataBaseEntries)
-    .then((dbResults) => {
-      console.log(`${dbResults.length} entries saved in DataBase`);
-    })
-    .catch((err) => {
-      console.error(err);
-    })
-    .then(() => {
-      mongoose.connection.close(() => {
-        process.exit(0);
-      });
-    });
+const csvWriter = createCsvWriter({
+  path: `${__dirname}/data/test.csv`,
+  header: [
+    { id: 'room_id', title: 'room_id' },
+    { id: 'photo_url', title: 'photo_url' },
+    { id: 'verified', title: 'verified' },
+    { id: 'description', title: 'description' },
+  ],
 });
+
+const records = data;
+
+csvWriter
+  .writeRecords(records) // returns a promise
+  .then(() => {
+    console.log('...Done');
+  });
