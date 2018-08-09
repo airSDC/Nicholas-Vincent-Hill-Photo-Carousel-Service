92522743bcaf286aab5526f30146156a6efefd23
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-09 16:30:44 -0700

    Implement createWriteStream generation attempt

--- generate.js
+++ generate.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const IMG_URL = 'https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-';
 const start = 1;
-const MAX_ID_RANGE = start + 500000;
+const MAX_ID_RANGE = start + 1000;
 const MAX_IMG_RANGE = 10;
 
 const getRandomIntInclusive = (min, max) => {
@@ -13,8 +13,11 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - minRounded + 1)) + minRounded;
 };
 
+console.time('data');
+
 const generateData = () => {
   let csv = '';
+  const stream = fs.createWriteStream('data/data.csv');
   for (let id = start; id <= MAX_ID_RANGE; id += 1) {
     for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
       const name = `room${id}`;
@@ -22,11 +25,11 @@ const generateData = () => {
       const verified = !Math.floor(Math.random() * 2);
       const desc = faker.lorem.sentence();
       csv += `${id},${name},${url},${verified},${desc},\n`;
+      stream.write(csv);
     }
   }
-  return csv;
+  stream.end();
 };
 
-fs.writeFile('data/data.csv', generateData(), (err) => {
-  if (err) throw err;
-});
+setTimeout(() => generateData(), 0);
+console.timeEnd('data');
