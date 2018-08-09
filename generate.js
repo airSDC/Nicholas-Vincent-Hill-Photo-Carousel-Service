f92472d615e6b0e9a477b625bf7e997c9c1aaef1
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-09 11:08:04 -0700

    Refactor generate.js

--- generate.js
+++ generate.js
@@ -1,8 +1,7 @@
 const faker = require('faker');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const IMG_URL = 'https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-';
-
+const start = 1;
 const MAX_ID_RANGE = 100000;
 const MAX_IMG_RANGE = 10;
 
@@ -11,36 +10,15 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - minRounded + 1)) + minRounded;
 };
 
-console.log('starting data generation....\n');
-
-const data = [];
-
-for (let id = 1; id <= MAX_ID_RANGE; id += 1) {
+for (let id = start; id <= MAX_ID_RANGE; id += 1) {
   for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
     const photo = {
-      room_id: id,
-      photo_url: `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`,
+      id,
+      name: `room${id}`,
+      url: `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`,
       verified: !Math.floor(Math.random() * 2),
-      description: faker.lorem.sentence(),
+      desc: faker.lorem.sentence(),
     };
-    data.push(photo);
+    console.log(`${photo.id},${photo.name},${photo.url},${photo.verified},${photo.desc}`);
   }
 }
-
-const csvWriter = createCsvWriter({
-  path: `${__dirname}/data/test.csv`,
-  header: [
-    { id: 'room_id', title: 'room_id' },
-    { id: 'photo_url', title: 'photo_url' },
-    { id: 'verified', title: 'verified' },
-    { id: 'description', title: 'description' },
-  ],
-});
-
-const records = data;
-
-csvWriter
-  .writeRecords(records) // returns a promise
-  .then(() => {
-    console.log('...Done');
-  });
