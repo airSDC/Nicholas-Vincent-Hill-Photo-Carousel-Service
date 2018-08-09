bb2aecff2c1d0992d901805cac621749ab6d7cee
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-09 15:28:14 -0700

    Bugfix and optimize data generation script

--- generate.js
+++ generate.js
@@ -1,8 +1,11 @@
+// generate primary key table
+
 const faker = require('faker');
+const fs = require('fs');
 
 const IMG_URL = 'https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-';
 const start = 1;
-const MAX_ID_RANGE = 100000;
+const MAX_ID_RANGE = start + 500000;
 const MAX_IMG_RANGE = 10;
 
 const getRandomIntInclusive = (min, max) => {
@@ -10,15 +13,20 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - minRounded + 1)) + minRounded;
 };
 
-for (let id = start; id <= MAX_ID_RANGE; id += 1) {
-  for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
-    const photo = {
-      id,
-      name: `room${id}`,
-      url: `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`,
-      verified: !Math.floor(Math.random() * 2),
-      desc: faker.lorem.sentence(),
-    };
-    console.log(`${photo.id},${photo.name},${photo.url},${photo.verified},${photo.desc}`);
+const generateData = () => {
+  let csv = '';
+  for (let id = start; id <= MAX_ID_RANGE; id += 1) {
+    for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
+      const name = `room${id}`;
+      const url = `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`;
+      const verified = !Math.floor(Math.random() * 2);
+      const desc = faker.lorem.sentence();
+      csv += `${id},${name},${url},${verified},${desc},\n`;
+    }
   }
-}
+  return csv;
+};
+
+fs.writeFile('data/data.csv', generateData(), (err) => {
+  if (err) throw err;
+});
