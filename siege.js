a773964e746dbbe3a3da6f746054c378ca664263
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-17 11:33:37 -0700

    Implement stress test procedure with new relic

--- siege.js
+++ siege.js
@@ -1,18 +1,18 @@
 const siege = require('siege');
 
-const randNums = [];
+const randomNumbers = [];
 
 const random = (min, max) => Math.floor(Math.random() * max) + min;
 
 const arrSize = 150000;
-for (let i = 0; i < arrSize; i++) {
-  randNums.push(random(1, 10000000));
+for (let i = 0; i < arrSize; i += 1) {
+  randomNumbers.push(random(1, 10000000));
 }
 
 let sieger = siege().on(3004);
 
-for (let i = 0; i < randNums.length; i++) {
-  sieger = sieger.for(1).times.get(`/api/rooms/${randNums[i]}`);
+for (let i = 0; i < randomNumbers.length; i += 1) {
+  sieger = sieger.for(1).times.get(`/api/rooms/${randomNumbers[i]}/photos`);
 }
 
 sieger.attack();
