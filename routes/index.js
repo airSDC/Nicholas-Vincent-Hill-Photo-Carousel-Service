21a5d3b02b9e15e03ad71d971b23d4c565ecdc44
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-18 12:04:22 -0700

    Implement redis caching

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const ctrl = require('./../controllers');
 
+const cache = (req, res, next) => {
+  const org = req.query.org;
+  client.get(org, (err, data) => {
+    if (err) throw err;
+
+    if (data != null) {
+      res.send(respond(org, data));
+    } else {
+      next();
+    }
+  });
+};
+
 const router = express.Router();
 
-router.get('/rooms/:id/photos/', ctrl.photos.get);
+router.get('/rooms/:id/photos/', cache, ctrl.photos.get);
 router.post('/rooms/:id/photos/', ctrl.photos.post);
 router.put('/rooms/:id/photos/', ctrl.photos.put);
 router.delete('/rooms/:id/photos/', ctrl.photos.delete);
